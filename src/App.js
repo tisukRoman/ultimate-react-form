69014874716eaeba55613a86b25cb841886fbe22
tisukRoman <tisukroman@gmail.com>
2022-07-26 19:23:56 +0300

    fixing deployment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Step1 from './components/Step1';
 import Step2 from './components/Step2';
@@ -9,14 +9,14 @@ function App() {
   return (
     <>
       <Header />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path='/' element={<Step1 />} />
           <Route path='/step2' element={<Step2 />} />
           <Route path='/step3' element={<Step3 />} />
           <Route path='/result' element={<Result />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
